30403602091bcdf755747f4912f281999c465643
Soonmyung Hwang <soonmyung.hwang.cohort225@gmail.com>
2022-11-02 11:06:37 -0700

    change css

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
   const cartClassName = cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
   return(
     <header>
-          <Link to="/"><h2>Pic Some</h2></Link>
+          <Link to="/"><h2>Photos</h2></Link>
           <Link to="/cart"><i className={`${cartClassName} ri-fw ri-2x`}></i></Link>
     </header>
   )
